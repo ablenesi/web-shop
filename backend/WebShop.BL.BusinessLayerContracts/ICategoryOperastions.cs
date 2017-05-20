2a874590ad73abb46d8df97246d7c60b25b682cc
Biro Gellert <gelllike@gmail.com>
2017-05-20 15:17:29 +0300

    Implement data layer interface

---
+++ backend/WebShop.BL.BusinessLayerContracts/ICategoryOperastions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using WebShop.BL.BusinessLayerContracts.DTOs;
+
+namespace WebShop.BL.BusinessLayerContracts
+{
+    interface ICategoryOperastions
+    {
+        void Create(CategoryDTO category);
+        IEnumerable<CategoryDTO> Get();
+    }
+}
---
+++ backend/WebShop.BL.BusinessLayerContracts/IOrderDetailOperations.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using WebShop.BL.BusinessLayerContracts.DTOs;
+
+namespace WebShop.BL.BusinessLayerContracts
+{
+    interface IOrderDetailOperations
+    {
+        void Create(OrderDetailsDTO orderDetail);
+        OrderDetailsDTO Get(int orderDetailId);
+    }
+}
