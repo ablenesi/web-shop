6fcddef1f4637e38f9786dd86242ade43a9cb3ad
Attila Blenesi <ablenesi@gmail.com>
2017-05-22 00:39:31 +0300

    Add test for Category cration

---
+++ backend/WebShop.BL.BusinessLayerImpl.Tests/CategoryOperationsTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WebShop.DL.EFDataLayer;
+using WebShop.IoC.DI;
+using WebShop.BL.BusinessLayerContracts;
+using WebShop.BL.BusinessLayerContracts.DTOs;
+
+namespace WebShop.BL.BusinessLayerImpl.Tests
+{
+    [TestClass]
+    public class CategoryOperationsTest
+    {
+        [TestMethod]
+        public void Create_NewCategoryUnitOfWorkIOC_SaveIntoDB()
+        {
+
+            var category = new CategoryDTO
+            {
+                Name = String.Format("Test Category {0}", DateTime.Now.Ticks)
+            };
+
+            var unitOfWork = Resolver.Get<IUnitOfWork>();
+            
+            // Act
+            unitOfWork.CategoryOperations.Create(category);
+            unitOfWork.SaveChanges();
+
+            // Assert
+            using(var ctx = new WebShopContext())
+            {
+                var savedCategory = ctx.Categories.Single(x => x.Name == category.Name);
+                Assert.IsNotNull(savedCategory);
+            };
+        }
+    }
+}
