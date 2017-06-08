1bef7b43a7d1b20a9d1cdf2a22fb24ee4d6b1804
Attila Blénesi <battila@halcyonmobile.com>
2017-06-08 09:02:19 +0300

    Add networking related classes - Update main navigation

--- client/WebShop/app/src/main/java/edu/bbu/webshop/feature/order/OrdersFragment.kt
+++ client/WebShop/app/src/main/java/edu/bbu/webshop/feature/order/OrdersFragment.kt
@@ -1,15 +1,27 @@
 package edu.bbu.webshop.feature.order
 
+import android.arch.lifecycle.LifecycleFragment
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import edu.bbu.webshop.R
 
-class OrdersFragment : Fragment(){
+class OrdersFragment : LifecycleFragment() {
+
+
+    private lateinit var viewModel : OrdersViewModel;
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_order, container, false)
     }
+
+}
+
+class OrdersViewModel {
+
 }
---
+++ client/WebShop/app/src/main/java/edu/bbu/webshop/util/LiveDateCallAdapter.kt
@@ -0,0 +1,55 @@
+package edu.bbu.webshop.util
+
+import android.arch.lifecycle.LiveData
+import edu.bbu.webshop.api.ApiResponse
+import retrofit2.*
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import java.util.concurrent.atomic.AtomicBoolean
+
+class LiveDateCallAdapterFactory : CallAdapter.Factory() {
+
+    override fun get(returnType: Type?, annotations: Array<out Annotation>?, retrofit: Retrofit?): CallAdapter<*, *>? {
+        if (getRawType(returnType) != LiveData::class.java) {
+            return null;
+        }
+
+        val observableType = getParameterUpperBound(0, returnType as ParameterizedType)
+        if (getRawType(observableType) != ApiResponse::class.java) {
+            return null;
+        }
+
+        val bodyType = getParameterUpperBound(0, observableType as ParameterizedType)
+        return LiveDataCallAdapter<Any>(bodyType)
+    }
+
+}
+
+/**
+ * Adapter to converts [Call][retrofit2.Call] to [LiveData]<[ApiResponse]>.
+ */
+class LiveDataCallAdapter<R>(val resopnseType: Type) : CallAdapter<R, LiveData<ApiResponse<R>>> {
+
+    override fun responseType(): Type {
+        return resopnseType
+    }
+
+    override fun adapt(call: Call<R>?) = object : LiveData<ApiResponse<R>>() {
+        internal var started = AtomicBoolean(false)
+        override fun onActive() {
+            super.onActive()
+            if (started.compareAndSet(false, true)){
+                call?.enqueue(object : Callback<R>{
+                    override fun onResponse(call: Call<R>?, response: Response<R>?) {
+                        postValue(ApiResponse<R>(response))
+                    }
+
+                    override fun onFailure(call: Call<R>?, t: Throwable?) {
+                        postValue(ApiResponse<R>(t))
+                    }
+                })
+
+            }
+        }
+    }
+}
