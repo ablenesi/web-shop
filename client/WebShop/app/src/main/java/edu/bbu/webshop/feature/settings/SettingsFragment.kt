86fe836376a8017a06a09c120a3716925e9cc217
Unknown <gelllike@gmail.com>
2017-06-15 20:44:59 +0300

    Add save button to the settings screen

--- client/WebShop/app/src/main/java/edu/bbu/webshop/feature/settings/SettingsFragment.kt
+++ client/WebShop/app/src/main/java/edu/bbu/webshop/feature/settings/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package edu.bbu.webshop.feature.settings
 
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import edu.bbu.webshop.R
 
 class SettingsFragment : Fragment() {
@@ -13,4 +15,13 @@ class SettingsFragment : Fragment() {
         return inflater?.inflate(R.layout.fragment_settings, container, false)
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val ip : String = view?.findViewById<TextInputEditText>(R.id.server_ip)?.text.toString()
+        val port : String = view?.findViewById<TextInputEditText>(R.id.server_port)?.text.toString()
+        val button : Button? = view?.findViewById<Button>(R.id.save_button)
+        button?.setOnClickListener {
+            //save the ip and the  port
+        }
+    }
 }
